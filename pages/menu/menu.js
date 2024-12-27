386dc61e2190086a7f50bbe53f806214f145c2fa
Michael <michael_s.wilson@ymail.com>
2024-12-27 16:49:06 +0200

    landing video added

---
+++ pages/menu/menu.js
@@ -0,0 +1,188 @@
+const steakDishes = [
+  {
+      id: 1,
+      price: 25.99,
+      name: "Ribeye Steak",
+      ingredients: ["Ribeye steak", "Salt", "Black pepper", "Garlic", "Butter", "Rosemary"]
+  },
+  {
+      id: 2,
+      name: "Filet Mignon",
+      price: 34.99,
+      ingredients: ["Filet mignon", "Salt", "Black pepper", "Olive oil", "Butter", "Thyme"]
+  },
+  {
+      id: 3,
+      name: "New York Strip Steak",
+      price: 29.99,
+      ingredients: ["New York strip steak", "Salt", "Black pepper", "Garlic powder", "Butter"]
+  },
+  {
+      id: 4,
+      name: "T-Bone Steak",
+      price: 32.50,
+      ingredients: ["T-bone steak", "Salt", "Black pepper", "Olive oil", "Garlic", "Thyme"]
+  },
+  {
+      id: 5,
+      name: "Sirloin Steak",
+      price: 19.99,
+      ingredients: ["Sirloin steak", "Salt", "Black pepper", "Olive oil", "Onions", "Mushrooms"]
+  },
+  {
+      id: 6,
+      name: "Skirt Steak",
+      price: 21.99,
+      ingredients: ["Skirt steak", "Salt", "Black pepper", "Cumin", "Olive oil", "Lime juice"]
+  },
+  {
+      id: 7,
+      name: "Flank Steak",
+      price: 22.99,
+      ingredients: ["Flank steak", "Salt", "Black pepper", "Garlic", "Soy sauce", "Brown sugar"]
+  },
+  {
+      id: 8,
+      name: "Chuck Eye Steak",
+      price: 18.99,
+      ingredients: ["Chuck eye steak", "Salt", "Black pepper", "Garlic powder", "Butter"]
+  },
+  {
+      id: 9,
+      name: "Porterhouse Steak",
+      price: 37.99,
+      ingredients: ["Porterhouse steak", "Salt", "Black pepper", "Butter", "Rosemary"]
+  },
+  {
+      id: 10,
+      name: "Steak Frites",
+      price: 28.99,
+      ingredients: ["Skirt steak or ribeye", "Salt", "Black pepper", "Fried potatoes", "Herbs"]
+  },
+  {
+      id: 11,
+      name: "Chicken Fried Steak",
+      price: 15.99,
+      ingredients: ["Tenderized beef steak", "Flour", "Eggs", "Milk", "Breadcrumbs", "Gravy"]
+  },
+  {
+      id: 12,
+      name: "Duck Breast Steak",
+      price: 27.99,
+      ingredients: ["Duck breast", "Salt", "Black pepper", "Honey", "Thyme"]
+  },
+  {
+      id: 13,
+      name: "Pork Chop Steak",
+      price: 18.50,
+      ingredients: ["Pork chop", "Salt", "Black pepper", "Garlic", "Apple sauce"]
+  },
+  {
+      id: 14,
+      name: "Lamb Chop Steak",
+      price: 32.99,
+      ingredients: ["Lamb chops", "Salt", "Black pepper", "Garlic", "Rosemary", "Olive oil"]
+  },
+  {
+      id: 15,
+      name: "Tuna Steak",
+      price: 24.99,
+      ingredients: ["Tuna steak", "Salt", "Black pepper", "Olive oil", "Lemon juice"]
+  },
+  {
+      id: 16,
+      name: "Salmon Steak",
+      price: 22.99,
+      ingredients: ["Salmon steak", "Salt", "Black pepper", "Olive oil", "Garlic", "Lemon"]
+  },
+  {
+      id: 17,
+      name: "Swordfish Steak",
+      price: 26.99,
+      ingredients: ["Swordfish steak", "Salt", "Black pepper", "Olive oil", "Garlic"]
+  },
+  {
+      id: 18,
+      name: "Beef Wellington",
+      price: 49.99,
+      ingredients: ["Beef tenderloin", "Puff pastry", "Mushrooms", "Prosciutto", "Egg yolk"]
+  },
+  {
+      id: 19,
+      name: "Venison Steak",
+      price: 28.99,
+      ingredients: ["Venison steak", "Salt", "Black pepper", "Garlic", "Juniper berries"]
+  },
+  {
+      id: 20,
+      name: "Bison Steak",
+      price: 30.99,
+      ingredients: ["Bison steak", "Salt", "Black pepper", "Garlic", "Butter"]
+  },
+  {
+      id: 21,
+      name: "Ostrich Steak",
+      price: 29.50,
+      ingredients: ["Ostrich steak", "Salt", "Black pepper", "Herbs", "Olive oil"]
+  },
+  {
+      id: 22,
+      name: "Pork Tenderloin",
+      price: 19.99,
+      ingredients: ["Pork tenderloin", "Salt", "Black pepper", "Garlic", "Olive oil"]
+  },
+  {
+      id: 23,
+      name: "Lobster Tail Steak",
+      price: 39.99,
+      ingredients: ["Lobster tail", "Butter", "Garlic", "Lemon juice", "Parsley"]
+  },
+  {
+      id: 24,
+      name: "Grilled Sirloin with Chimichurri",
+      price: 26.99,
+      ingredients: ["Sirloin steak", "Chimichurri sauce", "Garlic", "Parsley", "Olive oil", "Red pepper flakes"]  
+  },
+  {
+      id: 25,
+      name: "Alaskan Halibut Steak",
+      price: 37.99,
+      ingredients: ["Halibut steak", "Salt", "Black pepper", "Lemon", "Garlic", "Butter"]
+  },
+  {
+      id: 26,
+      name: "Chicken Steak",
+      price: 15.99,
+      ingredients: ["Chicken breast", "Salt", "Black pepper", "Olive oil", "Herbs"]
+  },
+  {
+      id: 27,
+      name: "Rabbit Steak",
+      price: 24.99,
+      ingredients: ["Rabbit leg", "Salt", "Pepper", "Garlic", "Rosemary"]
+  },
+  {
+      id: 28,
+      name: "Grilled Quail",
+      price: 28.99,
+      ingredients: ["Quail", "Salt", "Black pepper", "Olive oil", "Thyme"]
+  },
+  {
+      id: 29,
+      name: "Pheasant Breast Steak",
+      price: 26.50,
+      ingredients: ["Pheasant breast", "Salt", "Black pepper", "Butter", "Herbs"]
+  },
+  {
+      id: 30,
+      name: "Hanger Steak",
+      price: 22.50,
+      ingredients: ["Hanger steak", "Salt", "Black pepper", "Garlic", "Butter"]
+  },
+  {
+      id: 31,
+      name: "Buffalo Steak",
+      price: 31.50,
+      ingredients: ["Buffalo steak", "Salt", "Black pepper", "Garlic", "Herbs"]
+  }
+];
