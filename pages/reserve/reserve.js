9bdefaddf9e4f24d2c0ba7680471fa4a4ac170fc
Michael <michael_s.wilson@ymail.com>
2024-12-28 19:59:30 +0200

    prepared and styled reserve page

---
+++ pages/reserve/reserve.js
@@ -0,0 +1,6 @@
+// https://docs.google.com/forms/d/e/1FAIpQLSeXlFrcOfm11AgB1KI2B009-CsGQgXRPwblfv3VdHMaauyj6g/formResponse
+// name = entry.1942114490
+// phone = entry.1709168447
+// how many persons= entry.169645066
+
+//https://docs.google.com/forms/d/e/1FAIpQLSeXlFrcOfm11AgB1KI2B009-CsGQgXRPwblfv3VdHMaauyj6g/formResponse?entry.1942114490=kjkj&entry.1709168447=01007015953&entry.169645066=2-persons
